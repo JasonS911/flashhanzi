ffd1e00143b3527d70aef30a38530ce67361011b
Jason <shij1245@gmail.com>
2025-06-16 09:49:43 -0400

    launcher bug fix

--- android/app/src/main/kotlin/com/example/flashhanzi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flashhanzi/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.axoncode.flashhanzi
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge layout
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
